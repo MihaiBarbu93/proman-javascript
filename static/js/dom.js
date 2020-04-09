7dd7abebb872ce9861584ea08b3602821b5b7cfa
Gheboianu Bogdan <bogdan.gheboianu.2013@gmail.com>
2020-04-09 14:44:15 +0300

    Added: show cards

--- static/js/dom.js
+++ static/js/dom.js
@@ -29,7 +29,7 @@ export let dom = {
                         <span class="board-title" id="board-title-` +
               board.id +
               `" contenteditable="true">${board.title}</span>
-                        <button class="board-add">Add Card</button>
+                        <button class="board-add" id="add-card-btn-${board.id}">Add Card</button>
                         <button id="${board.id}" class="board-toggle btn btn-link" data-toggle="collapse" data-target="#collapse${board.id}" aria-expanded="false" aria-controls="collapseOne">
                         <i class="fas fa-chevron-down"></i></button>
                     </div>
@@ -44,20 +44,19 @@ export let dom = {
             
         }
 
-
-        let expand_buttons = document.getElementsByClassName("board-toggle")
+        
         let titles = document.getElementsByClassName("board-title")
         for (let title of titles){
             title.addEventListener('blur', function(e){
 
                 dom.updateBoardTitle(e.target.id)})}
 
-        for (let expand_button of expand_buttons){
-                expand_button.addEventListener('click', function(e) {
-                    
-                    dom.loadCards(parseInt(e.target.id))
-                })
-            }
+        const addCardButtons = document.getElementsByClassName('board-add');
+        for (let btn of addCardButtons){
+            btn.addEventListener('click', function(){
+                dom.addCard(btn);
+            })
+        }
     },
 
     showColumns : function (boards) {
@@ -67,33 +66,46 @@ export let dom = {
 
 
         let expand_buttons = document.getElementsByClassName("board-toggle")
+    
+        
         for (let expand_button of expand_buttons) {
             expand_button.addEventListener('click', async function (e) {
+            
+                if(e.target.className == 'fas fa-chevron-down'){
+                    var boardId = parseInt(e.target.parentNode.id);
+                }else{
+                    var boardId = parseInt(e.target.id);
+                }
+
+                dom.loadCards(boardId)
+
 
                 let allStatuses = []
-
                 fetch('/get-statuses')
                     .then((response) => response.json())
                     .then(responseJson => {
 
                         let boardsCollection = document.getElementsByClassName("board");
                         for (let board of boardsCollection) {
-                            if (board.id.substr(-1) == expand_button.id) {
+                            if (board.id.substr(-1) == boardId) {
                                 for (let i = 0; i < responseJson.length; i++) {
                                     if (parseInt(responseJson[i]["board_id"]) == 0 || board.id.substr(-1) == responseJson[i]["board_id"]) {
                                         allStatuses.push(responseJson[i]["title"]);
+                                        
                                     }
                                 }
                             }
                         }
 
+                        allStatuses = allStatuses.reverse()
                         for (let board of boards) {
-                            if (board.id == expand_button.id) {
+                            if (board.id == boardId) {
                                 
                                 boardColumnContainers[board.id - 1].innerHTML = "";
                                 for (let i = 0; i < allStatuses.length; i++) {
                                     let boardColumn = `<div class="board-column" id="board-column`+responseJson[i]['id']+`">
                                 <div class="board-column-title">${allStatuses[i]}</div>
+                                <div class="board-column-content" id="${allStatuses[i]}"></div>
                                 </div>`;
 
                                     boardColumnContainers[board.id - 1].innerHTML += boardColumn;
@@ -132,10 +144,19 @@ export let dom = {
     },
 
     loadCards: function (boardId) {
-        console.log(boardId)
-            // dom.showCards(data, boardId)
-        // retrieves cards and makes showCards called
+         // retrieves cards and makes showCards called
+        const requestCards = new XMLHttpRequest();
+        requestCards.onreadystatechange = function(){
+            if(requestCards.readyState == 4 && requestCards.status == 200){
+                let cards = JSON.parse(requestCards.response);
+                dom.showCards(cards, boardId);
+            }
+        }
+        requestCards.open('GET', `/get-cards/${boardId}`, true);
+        requestCards.send();
+
     },
+
     updateBoardTitle: function (boardId){
             let boardTitle = boardId;
             let titleValue = document.getElementById(boardTitle);
@@ -163,11 +184,43 @@ export let dom = {
     },
 
     showCards: function (cards, boardId) {
-        let card_list = document.getElementById(`card_list_${boardId}`)
-        return cards
+        const boardColumns = document.getElementsByClassName('board-columns')[boardId - 1]
+
+        for(let boardCol of boardColumns.childNodes){
+            try{
+                var colContentId = boardCol.childNodes[3].id;
+            }
+            catch{
+                continue;
+            }
+            for(let card of cards){
+                if(colContentId == card['status_id']){
+                    let cardElement = document.createElement('div');
+                    cardElement.setAttribute('class', 'card');
+                    let cardRemove = document.createElement('div');
+                    cardRemove.setAttribute('class', 'card-remove');
+                    let i = document.createElement('i');
+                    i.setAttribute('class', 'fas fa-trash-alt');
+                    cardRemove.appendChild(i);
+                    cardElement.appendChild(cardRemove);
+                    let cardTitle = document.createElement('div');
+                    cardTitle.setAttribute('class', 'card-title');
+                    cardTitle.textContent = card['title'];
+                    cardElement.appendChild(cardTitle);
+                    console.log(cardElement)
+                    boardCol.childNodes[3].appendChild(cardElement);
+                    console.log(boardCol)
+                }
+            }
+        }
         // shows the cards of a board
         // it adds necessary event listeners also
     },
+
+    addCard: function(btnPressed){
+        console.log(btnPressed)
+    },
+
     addBoardModal: function () {
         let addButton=document.getElementById('new_board_btn')
         let modal = document.getElementById("myModal")
